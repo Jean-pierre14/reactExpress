d13d6e08dd948c059feb61a150469506f72f5cf1
User <chiruzabisimwa@gmail.com>
2021-08-07 21:31:21 +0200

    styling

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import NavLink from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 
 class Navbar extends React.Component {
     render() {
         return (
             <nav>
-                <NavLink to="/">Jean-Pierre14</NavLink>
-                <ul>
-                    <li><NavLink to="/home">Home</NavLink></li>
-                    <li><NavLink to="/home">Profil</NavLink></li>
-                    <li><NavLink to="/home">Logout</NavLink></li>
-                </ul>
+                <p>NavBar</p>
             </nav>
         )
     }
