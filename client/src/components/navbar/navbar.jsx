e3fc4431acbabf0a1cc6f836f9be5a517066a4e5
User <chiruzabisimwa@gmail.com>
2021-08-08 21:07:25 +0200

    NavBar susing Flex CSS3

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,34 +1,40 @@
 import React from "react"
+
+
 import "./navbar.css"
 
 class Navbar extends React.Component {
     render() {
         return (
-            <nav>
+            <nav className="NavBar">
                 <div className="NavBarLeft">
                     <span className="logo">
                         Jean-Pierre14
                     </span>
                 </div>
                 <div className="NavBarCenter">
-                    <input type="search" name="search" id="search" placeholder="Search" className="input" />
+                    <div className="SearchNavBar">
+                        <input type="search" name="search" id="search" placeholder="Search" className="NavBarinput" />
+                    </div>
                 </div>
                 <div className="NavBarRight">
-                    <img src="" alt="Profil image" className="profilImg" />
-                    <ul className="List">
-                        <li className="ListItem">
-                            <span>P</span>
-                            <span>Message</span>
-                        </li>
-                        <li className="ListItem">
-                            <span>P</span>
-                            <span>Profil</span>
-                        </li>
-                        <li className="ListItem">
-                            <span>P</span>
-                            <span>About</span>
-                        </li>
-                    </ul>
+                    <div className="Links">
+                        <img src="" alt="Profil image" className="profilImg" />
+                        <ul className="List">
+                            <li className="ListItem">
+                                <span>P</span>
+                                <span>Message</span>
+                            </li>
+                            <li className="ListItem">
+                                <span>P</span>
+                                <span>Profil</span>
+                            </li>
+                            <li className="ListItem">
+                                <span>P</span>
+                                <span>About</span>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         )
