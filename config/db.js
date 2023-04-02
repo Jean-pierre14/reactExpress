4dfb5520e1bb4de34d767de400575c50cd13a746
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-04-02 05:57:01 +0200

    Server config

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
-const { success, error } = require('consola')
-const mysql = require('mysql')
+const { success, error } = require("consola");
+const mysql = require("mysql");
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'crudajax'
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bisimwa_DB",
+});
 
 db.connect((err) => {
-    if (err) error({ message: `Error ${err}`, badge: true })
-    success({ message: `Database is connected`, badge: true })
-})
+  if (err) error({ message: `Error ${err}`, badge: true });
+  success({ message: `Database is connected`, badge: true });
+});
 
-module.exports = db
+module.exports = db;
